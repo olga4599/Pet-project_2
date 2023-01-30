fa82a75b4dc249a6f9cdae9003265a512738527d
olga4599 <73193270+olga4599@users.noreply.github.com>
2023-01-30 12:00:39 +0300

    Add files via upload

---
+++ cat.py
@@ -0,0 +1,12 @@
+﻿''' Конструктор Cat, который принимает из словаря значения name,gender, age '''
+class Cat:
+    def __init__(self, name="", gender="", age=0):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def init_from_dict(self, animal_dict):
+        self.name = animal_dict.get("name")
+        self.gender = animal_dict.get("gender")
+        self.age = animal_dict.get("age")
+
