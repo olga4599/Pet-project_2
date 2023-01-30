697ac3502762ab865f31d7929a83ec0f73a22f77
olga4599 <73193270+olga4599@users.noreply.github.com>
2023-01-30 12:26:16 +0300

    Add files via upload
    Второй вариант выполнения

---
+++ print_cat2.py
@@ -0,0 +1,7 @@
+from cat2 import Cat
+
+animal1 = Cat('Feliks', 'male', 2)
+print(f"Name: {animal1.name}. Gender: {animal1.gender}. Age: {animal1.age}")
+
+animal2 = Cat("Gosha", "male", 1)
+print(f"Name: {animal2.name}. Gender: {animal2.gender}. Age: {animal2.age}")
